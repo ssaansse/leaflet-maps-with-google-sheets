09236436db35ff69f36eeb39e9ea7b273ff6ded8
ssaansse <70260504+ssaansse@users.noreply.github.com>
2022-07-25 00:06:22 +0400

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -128,7 +128,7 @@ $(window).on('load', function() {
       if (point.Latitude !== '' && point.Longitude !== '') {
         var marker = L.marker([point.Latitude, point.Longitude], {icon: icon})
           .bindPopup("<b>" + point['Name'] + '</b><br>' +
-          (point['Image'] ? ('<span> point['Image'] </span><br>') : '') +
+          point['Image'] + '</b><br>' +
           point['Description']);
 
         if (layers !== undefined && layers.length !== 1) {
